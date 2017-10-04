6e991a22f19ebaf7d1760725a953af7ca6c2f2a4
anascko <anascko@gmail.com>
2017-10-04 16:13:05 +0300

    Update test-openstack-component-pipeline.groovy

--- test-openstack-component-pipeline.groovy
+++ test-openstack-component-pipeline.groovy
@@ -7,6 +7,9 @@
  *   FORMULAS_SOURCE
  *   SALT_OPTS
  *   STACK_DEPLOY_JOB
+ *   STACK_TEST_JOB
+ *   TEST_TEMPEST_TARGET
+ *   TEST_TEMPEST_PATTERN
 **/
 def common = new com.mirantis.mk.Common()
 def gerrit = new com.mirantis.mk.Gerrit()
@@ -14,14 +17,23 @@ def gerrit = new com.mirantis.mk.Gerrit()
 node {
     def cred = common.getCredentials(CREDENTIALS_ID, 'key')
     def gerritChange = gerrit.getGerritChange(cred.username, GERRIT_HOST, GERRIT_CHANGE_NUMBER, CREDENTIALS_ID, true)
-
+    def salt_master_url
+    def deployBuildParams
+    def PROJECT
+    def deployBuild
+ 
     stage('Trigger deploy job') {
-        build(job: STACK_DEPLOY_JOB, parameters: [
+        deployBuild = build(job: STACK_DEPLOY_JOB, parameters: [
             [$class: 'StringParameterValue', name: 'OPENSTACK_API_PROJECT', value: 'mcp-oscore'],
             [$class: 'StringParameterValue', name: 'STACK_TEST', value: ''],
             [$class: 'BooleanParameterValue', name: 'TEST_DOCKER_INSTALL', value: false]
         ])
     }
+    
+    // get salt master url
+        deployBuildParams = deployBuild.description.tokenize( ' ' )
+        salt_master_url = "http://${deployBuildParams[1]}:6969"
+        common.infoMsg("Salt API is accessible via ${salt_master_url}")
     
     // Run smoke tests
     stage('Run Smoke tests') {
