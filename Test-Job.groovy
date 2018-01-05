bfbb2c7d462a6013a18973e1f9ef25b40b20e03b
anascko <anascko@gmail.com>
2018-01-05 15:38:20 +0200

    Create Test-Job.groovy

---
+++ Test-Job.groovy
@@ -0,0 +1,30 @@
+#!/usr/bin/env groovy
+ 
+/**
+    Test jenkins Job
+    
+    def PROJECT_URL="https://github.com/tldr-pages/tldr"
+    def PROJECT="tldr"
+    def BRANCH="master"
+    
+*/
+
+node('builder') {    
+    stage('Get repository and install project') {
+        return sh script: '''
+            git clone ${PROJECT_URL} -b ${BRANCH}',
+            d ${PROJECT}
+            npm install
+        ''', returnStdout: true)
+    }
+
+if (RUN_TEST.toBoolean()){
+    stage('Run test') {
+        def result = sh (
+            script: 'tldr -V',
+            returnStdout: true
+        ).trim()
+    
+    }
+}
+}
